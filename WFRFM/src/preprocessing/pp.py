e448e174a53243aa65fef284130870cc9480f70c
jianzheli <2300017748@stu.pku.edu.cn>
2026-02-02 18:55:12 +0800

    for-pull

--- WFRFM/src/preprocessing/pp.py
+++ WFRFM/src/preprocessing/pp.py
@@ -40,22 +40,22 @@ def process_to_embedding(adata_control, adata_train,
         adata_test: Test.
     """
     if sample_rep == "X_pca":
-        sc.pp.normalize_total(adata_control, target_sum=1e4)
-        sc.pp.log1p(adata_control)
+        #sc.pp.normalize_total(adata_control, target_sum=1e4)
+        #sc.pp.log1p(adata_control)
         centered_pca(adata_control, n_comps=n_comps, method="scanpy")
         var = np.asarray(adata_control.uns["pca"]["variance"])
         adata_control.obsm[sample_rep+"_scaled"] = adata_control.obsm[sample_rep] / np.sqrt(var)
         print(np.std(np.array(adata_control.obsm[sample_rep + "_scaled"]), axis=0))
 
-        sc.pp.normalize_total(adata_train, target_sum=1e4)
-        sc.pp.log1p(adata_train)
+        #sc.pp.normalize_total(adata_train, target_sum=1e4)
+        #sc.pp.log1p(adata_train)
         project_pca(adata_train, ref_adata = adata_control, obsm_key_added="X_pca")
         adata_train.obsm[sample_rep+"_scaled"] = adata_train.obsm[sample_rep] / np.sqrt(var)
         print(np.std(np.array(adata_train.obsm[sample_rep + "_scaled"]), axis=0))
 
         if adata_test is not None:
-            sc.pp.normalize_total(adata_test, target_sum=1e4)
-            sc.pp.log1p(adata_test)
+            #sc.pp.normalize_total(adata_test, target_sum=1e4)
+            #sc.pp.log1p(adata_test)
             project_pca(adata_test, ref_adata = adata_control, obsm_key_added="X_pca")
             adata_test.obsm[sample_rep+"_scaled"] = adata_test.obsm[sample_rep] / np.sqrt(var)
             print(np.std(np.array(adata_test.obsm[sample_rep + "_scaled"]), axis=0))
@@ -148,6 +148,7 @@ def process_to_embedding(adata_control, adata_train,
         
 
     if condition_rep_dict is not None: 
+        print(f"using condition_rep_dict")
         temp = adata_train.obs[condition_keys].map(condition_rep_dict).values
         adata_train.obsm[condition_rep_keys] = convert_mixed_array_to_2d(temp) # 放到obsm中
         if adata_test is not None:
--- WFRFM/src/training/train.py
+++ WFRFM/src/training/train.py
@@ -101,7 +101,7 @@ def train_model(adata_control, adata_treated, adata_test,
             # logging.info(f"Epoch {i}: loss={loss.item():.3f}, vloss={vloss.item():.3f}, gloss={gloss.item():.3f}")
             # progress_bar.set_postfix({"loss": f"{loss.item():.3f}","vloss": f"{vloss.item():.3f}", "gloss": f"{gloss.item():.3f}"})
 
-        if (i+1) % 5000 == 0:
+        if (i+1) % 1000 == 0:
             if save_path is not None:
                 ckpt_path = f"{save_path}_epoch_{i+1}.pt"
                 torch.save({
